ba2aeefca845f0640cae0c34a82f215ebc8450d2
Marcos Rene <renemoura789@gmail.com>
2024-12-17 15:20:09 -0300

    refactor: add getters and setters entities

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
 import { AnswerRepository } from "../repositories/answer-repository";
 import { Answer } from "../entities/answer";
@@ -6,8 +5,8 @@ import { Answer } from "../entities/answer";
 const fakeAnswerRepository: AnswerRepository = {
   create: async (answer: Answer) => {
     return answer;
-  }
-}
+  },
+};
 
 test("create an answer", async () => {
   const useCase = new AnswerQuestionUseCase(fakeAnswerRepository);
