84b7a88757823184e3f9d82b58eba72bb5f73663
Marcos Rene <renemoura789@gmail.com>
2024-12-17 13:20:10 -0300

    feat: create new answer-respository repositories

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../entities/answer";
 
-test("create an answer", () => {
-  const useCase = new AnswerQuestionUseCase();
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return answer;
+  }
+}
 
-  const answer = useCase.execute({
+test("create an answer", async () => {
+  const useCase = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await useCase.execute({
     instructorId: "123",
     questionId: "456",
     content: "Hello world",
