cb393bc3a452b7b435fa42a76e06d8f3725add24
Marcos Rene <renemoura789@gmail.com>
2024-12-17 16:16:07 -0300

    chore: add eslint lib and setup

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,21 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswerRepository } from "../repositories/answer-repository";
-import { Answer } from "../entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswerRepository: AnswerRepository = {
   create: async (answer: Answer) => {
-    return answer;
+    return answer
   },
-};
+}
 
-test("create an answer", async () => {
-  const useCase = new AnswerQuestionUseCase(fakeAnswerRepository);
+test('create an answer', async () => {
+  const useCase = new AnswerQuestionUseCase(fakeAnswerRepository)
 
   const answer = await useCase.execute({
-    instructorId: "123",
-    questionId: "456",
-    content: "Hello world",
-  });
+    instructorId: '123',
+    questionId: '456',
+    content: 'Hello world',
+  })
 
-  expect(answer.content).toEqual("Hello world");
-});
+  expect(answer.content).toEqual('Hello world')
+})
