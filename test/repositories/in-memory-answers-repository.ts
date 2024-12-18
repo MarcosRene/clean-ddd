46536140ccf743526ddd1f1ecdefeff4f0323ee9
Marcos Rene <renemoura789@gmail.com>
2024-12-18 14:40:08 -0300

    feat: add delete question and answer use-cases

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,27 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public answers: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.answers.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.answers.push(answer)
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.answers.findIndex((item) => item.id === answer.id)
+
+    if (itemIndex === -1) {
+      return
+    }
+
+    this.answers.splice(itemIndex, 1)
   }
 }
