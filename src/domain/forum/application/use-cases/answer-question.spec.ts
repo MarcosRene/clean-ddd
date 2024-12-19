bc55749db503b1c90f29293f167a2c7d08ae92e8
Marcos Rene <renemoura789@gmail.com>
2024-12-19 18:36:17 -0300

    feat: add answer question attachments

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { AnswerQuestionUseCase } from './answer-question'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 
@@ -15,9 +16,19 @@ describe('Answer Question Use Case', () => {
       instructorId: '123',
       questionId: '456',
       content: 'Hello world',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryAnswersRepository.answers[0]).toEqual(result.value?.answer)
+    expect(
+      inMemoryAnswersRepository.answers[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(
+      inMemoryAnswersRepository.answers[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
