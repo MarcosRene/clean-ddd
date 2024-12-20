99c1d06f737f6d15006de7bcdb7f828452510614
Marcos Rene <renemoura789@gmail.com>
2024-12-20 14:50:55 -0300

    feat: add read notification use-case

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,7 +6,27 @@ export class InMemoryNotificationsRepository
 {
   public notifications: Notification[] = []
 
+  async findById(id: string) {
+    const notification = this.notifications.find(
+      (item) => item.id.toString() === id,
+    )
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification)
+  }
+
+  async save(notification: Notification) {
+    const itemIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.notifications[itemIndex] = notification
   }
 }
